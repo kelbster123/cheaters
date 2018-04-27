c647329566180f73995fbffd6e15100535cfec61
kelbster123 <kelby.erickson@gmail.com>
2018-04-26 19:25:17 -0500

    Reading through Map to produce meaningful output

--- cheaters.java
+++ cheaters.java
@@ -1,23 +1,57 @@
 package assignment7;
+/*
+ * EE422C Project 4 submission by
+ * Varun Prabhu
+ * vp6793
+ * 15465
+ * Kelby Erickson
+ * kde528
+ * 15495
+ * Spring 2018
+ */
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-/**
- * Created by varun on 4/26/2018.
- */
 public class cheaters {
-
-
+	
+	public static class CollisionObject {
+		int numCollisions;
+		String file1;
+		String file2;
+		
+		public CollisionObject(int numCol, String f1, String f2) {
+			numCollisions = numCol;
+			file1 = f1;
+			file2 = f2;
+		}
+	}
+	
+	public static class CollisionComparator implements Comparator<CollisionObject> {
+		public int compare(CollisionObject c1, CollisionObject c2) {
+			if (c1.numCollisions > c2.numCollisions) {
+				return -1;
+			} else if (c1.numCollisions < c2.numCollisions) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+	
     public static void main(String[] args) {
-        if(args.length != 2) {
+        if (args.length != 2 && args.length != 3) {
             System.out.println("Wrong number of input variables");
             System.exit(0);
         }
 
         final File directory = new File(args[0]);
         int n = Integer.parseInt(args[1]);
+        int limit = 0;
+        if (args.length == 3) {
+        	limit = Integer.parseInt(args[2]);
+        }
 
         LinkedHashMap<String, LinkedList<Integer>> similarities = new LinkedHashMap<>(1500000, .75f, false);
 
@@ -25,34 +59,33 @@ public class cheaters {
 
         int numberOfFiles = 0;
 
-
-        for(File f : directory.listFiles()) {
+        for (final File f : directory.listFiles()) {
             String name = f.getName();
 
             filesToName.put(numberOfFiles, name);
             Scanner s;
 
-            try{
+            try {
                 s = new Scanner(f);
 
                 Queue<String> buffer = new LinkedList<String>();
 
-                while(s.hasNext() && buffer.size() < n) {
+                while (s.hasNext() && buffer.size() < n) {
                     String inputWord = s.next();
+                    
                     String formattedWord = makeCaps(inputWord);
                     buffer.add(formattedWord);
                 }
 
-                while(buffer.size() == n) {
+                while (buffer.size() == n) {
                     String phrase = "";
                     for(String word : buffer) {
                         phrase += word;
                     }
-
-
-                    if(similarities.containsKey(phrase)) {
+                    
+                    if (similarities.containsKey(phrase)) {
                         similarities.get(phrase).add(numberOfFiles);
-                    }else{
+                    } else {
                         LinkedList<Integer> newPhrase = new LinkedList<Integer>();
                         newPhrase.add(numberOfFiles);
                         similarities.put(phrase, newPhrase);
@@ -60,27 +93,62 @@ public class cheaters {
 
                     buffer.remove();
 
-                    if(s.hasNext()) {
+                    if (s.hasNext()) {
                         String nextWord = s.next();
                         nextWord = makeCaps(nextWord);
                         buffer.add(nextWord);
                     }
-
                 }
-                //delete this
+                
                 numberOfFiles++;
             }
             catch (FileNotFoundException e) {
                 System.out.println("File not found");
             }
-
-
-
         }
-
+        
+        int[][] collisions = new int[numberOfFiles][numberOfFiles];
+        
+        for (String key : similarities.keySet()) {
+        	LinkedList<Integer> list = similarities.get(key);
+        	while (list.size() > 1) {
+        		int firstElem = list.remove();
+        		Iterator<Integer> it = list.iterator();
+        		HashSet<Integer> alreadyChecked = new HashSet<>();
+        		while (it.hasNext()) {
+        			int otherElem = it.next();
+        			if (otherElem != firstElem && !alreadyChecked.contains(otherElem)) {
+        				alreadyChecked.add(otherElem);
+        				int minElem;
+        				int maxElem;
+        				if (firstElem < otherElem) {
+        					minElem = firstElem;
+        					maxElem = otherElem;
+        				} else {
+        					minElem = otherElem;
+        					maxElem = firstElem;
+        				}
+        				collisions[minElem][maxElem]++;
+        			}
+        		}
+        	}
+        }
+        
+        Queue<CollisionObject> heap = new PriorityQueue<>(numberOfFiles*numberOfFiles, new CollisionComparator());
+        for (int row = 0; row < numberOfFiles; row++) {
+        	for (int col = row + 1; col < numberOfFiles; col++) {
+        		if (collisions[row][col] > limit) {
+        			heap.add(new CollisionObject(collisions[row][col], filesToName.get(row), filesToName.get(col)));
+        		}
+        	}
+        }
+        
+        while (!heap.isEmpty()) {
+        	CollisionObject colObj = heap.remove();
+        	System.out.println(colObj.numCollisions + ":\t" + colObj.file1 + ",\t" + colObj.file2);
+        }
     }
-
-
+    
     public static String makeCaps(String input) {
         String result = "";
         char[] str = input.toCharArray();
