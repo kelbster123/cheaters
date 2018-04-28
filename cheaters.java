ec5079aeb848f3a4bb1be188bb209b586b4573c0
varunprabhu <varunprabhu.tx@gmail.com>
2018-04-28 16:32:06 -0500

    added comments to cheaters

--- cheaters.java
+++ cheaters.java
@@ -42,59 +42,80 @@ public class cheaters {
 	
     public static void main(String[] args) {
     	long time = System.nanoTime();
+    	
+    	//this is when the user input is not correct and the program will not work
         if (args.length != 2 && args.length != 3) {
             System.out.println("Wrong number of input variables");
             System.exit(0);
         }
 
+        //this is the path to all of the text files to check
         final File directory = new File(args[0]);
+        
+        //this is the number of words in each phrase to check
         int n = Integer.parseInt(args[1]);
         int limit = 0;
+        
+        //optional third user input which specifies the lower limit of the similarities needed for files to be considered similar
+        //and shown on screen. This is defaulted to 0 (at least 1 similarity) if there is no user input.
         if (args.length == 3) {
         	limit = Integer.parseInt(args[2]);
         }
         
         
         
-        
+        //This is where we will store the similarities. Each 6 word phrase is mapped to a linked list of integers representing the 
+        //file in which this phrase was found. 
         LinkedHashMap<String, LinkedList<Integer>> similarities = new LinkedHashMap<>(1500000, .75f, false);
+        
+        //This hashmap maps the file number to the string representing its name, integers are used in the similarities map for 
+        //efficiency purposes and we use this to get the actual file name of each file.
         HashMap<Integer, String> filesToName = new HashMap<>(1500, .75f);
+        
+        //this value is the number that we assign to each file and is incremented each time we finish processing a file
         int numberOfFiles = 0;
 
         for (final File f : directory.listFiles()) {
             String name = f.getName();
 
+            //stores the file number to name in the names hashmap
             filesToName.put(numberOfFiles, name);
             Scanner s;
 
             try {
                 s = new Scanner(f);
-
+                
+                
+                //this queue is used to store the n length phrases that we are processing
                 Queue<String> buffer = new LinkedList<String>();
-
+              
+                //this fills up the buffer for the first time before we begin processing the data
                 while (s.hasNext() && buffer.size() < n) {
                     String inputWord = s.next();
                     
+                    //gets rid of punctuation and makes the word all caps before entering into buffer
                     String formattedWord = makeCaps(inputWord);
                     buffer.add(formattedWord);
                 }
 
+                //this loop will execute once for every n word phrase in the current file
                 while (buffer.size() == n) {
                     String phrase = "";
+                    //this creates the n word phrase that we will be hashing
                     for(String word : buffer) {
                         phrase += word;
                     }
                     
-                    if (similarities.containsKey(phrase)) {
+                    if (similarities.containsKey(phrase)) {//if this phrase has been seen before, add the file number to the linked list
                         similarities.get(phrase).add(numberOfFiles);
-                    } else {
+                    } else {//if this is the first time that this phrase has been seen, create a new linked list and add it to the map
                         LinkedList<Integer> newPhrase = new LinkedList<Integer>();
                         newPhrase.add(numberOfFiles);
                         similarities.put(phrase, newPhrase);
                     }
 
+                    //removes the first word of the phrase and adds the next word in file to buffer to create the next n word phrase if possible
                     buffer.remove();
-
                     if (s.hasNext()) {
                         String nextWord = s.next();
                         nextWord = makeCaps(nextWord);
@@ -148,6 +169,7 @@ public class cheaters {
         // Print list of collision calculations to the console
         
         Queue<CollisionObject> heap = new PriorityQueue<>(numberOfFiles*numberOfFiles, new CollisionComparator()); // heap to sort pairs of files
+        
         for (int row = 0; row < numberOfFiles; row++) {
         	for (int col = row + 1; col < numberOfFiles; col++) {
         		if (collisions[row][col] > limit) {
@@ -157,6 +179,7 @@ public class cheaters {
         }
         while (!heap.isEmpty()) {
         	CollisionObject colObj = heap.remove(); // pair of files with next highest number of collisions
+        
         	System.out.println(colObj.numCollisions + ":\t" + colObj.file1 + ",\t" + colObj.file2);
         }
     }
